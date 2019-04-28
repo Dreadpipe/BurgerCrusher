8f053aeea6c8bb5d53236cfbfd4104363f104c4f
Dreadpipe <benharris83@gmail.com>
2019-04-28 15:21:13 -0500

    updated ORM, controllers

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,29 +1,29 @@
 // Dependencies
-var express = require("express");
+const express = require("express");
 
-var router = express.Router();
+const router = express.Router();
 
 // Import the model (burgerModel.js) to use its database functions.
-var burger = require("../models/burgerModel");
+const burger = require("../models/burgerModel");
 
 // ROUTES (default "/" = GET, selectAll = GET, insertOne = POST, updateOne = PUT)
 
 // DEFAULT - SELECT ALL?
 router.get("/", function (req, res) {
-    cat.all(function (data) {
+    burger.all(function (data) {
         var hbsObject = {
-            cats: data
+            burger: data
         };
         console.log(hbsObject);
         res.render("index", hbsObject);
     });
 });
 // INSERT ONE
-router.post("/api/cats", function (req, res) {
-    cat.create([
-        "name", "sleepy"
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "burger_name"
     ], [
-        req.body.name, req.body.sleepy
+        req.body.name
     ], function (result) {
         // Send back the ID of the new quote
         res.json({
@@ -33,13 +33,13 @@ router.post("/api/cats", function (req, res) {
 });
 
 // UPDATE ONE
-router.put("/api/cats/:id", function (req, res) {
+router.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
 
-    cat.update({
-        sleepy: req.body.sleepy
+    burger.update({
+        crushed: true
     }, condition, function (result) {
         if (result.changedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404
